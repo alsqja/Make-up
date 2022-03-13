d1ff0409066397c9acd0d1c8af07c4061f4136bc
minseok <kms9887@naver.com>
2022-03-13 17:27:27 +0900

    [Add] Add Entity for Jpa Jpa 사용을 위한 Entity 추가 - user - post - file

---
+++ server/src/main/java/ICTPrj/server/domain/entity/User.java
@@ -0,0 +1,36 @@
+package ICTPrj.server.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String password;
+
+    @Column
+    private String email;
+
+    @Column
+    private String profile;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user")
+    List<Post> posts = new ArrayList<>();
+}
