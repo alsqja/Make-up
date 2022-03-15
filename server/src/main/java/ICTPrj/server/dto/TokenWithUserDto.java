d10c9f4c607289b081780f7da497c93d100468a6
minseok <kms9887@naver.com>
2022-03-15 10:22:58 +0900

    [Add] Add SignIn 로그인 구현

---
+++ server/src/main/java/ICTPrj/server/dto/TokenWithUserDto.java
@@ -0,0 +1,13 @@
+package ICTPrj.server.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenWithUserDto {
+    private String accessToken;
+    private UserDto user;
+}
---
+++ server/src/main/java/ICTPrj/server/service/SignInService.java
@@ -0,0 +1,38 @@
+package ICTPrj.server.service;
+
+import ICTPrj.server.domain.entity.User;
+import ICTPrj.server.domain.repository.UserRepository;
+import ICTPrj.server.dto.TokenDto;
+import ICTPrj.server.dto.TokenWithUserDto;
+import ICTPrj.server.dto.UserDto;
+import ICTPrj.server.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class SignInService {
+    private final UserRepository userRepository;
+    private final TokenProvider tokenProvider;
+
+    public TokenWithUserDto signIn(UserDto userDto) {
+        User user = userRepository.findByEmail(userDto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("없는 사용자입니다."));
+
+        userDto.setId(user.getId());
+        if(!userDto.getPassword().equals(user.getPassword())){
+            throw new IllegalArgumentException("비밀번호를 확인하세요.");
+        }
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDto.getEmail(), "", null);
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+        return TokenWithUserDto.builder()
+                .accessToken(tokenDto.getAccessToken())
+                .user(userDto)
+                .build();
+    }
+}
