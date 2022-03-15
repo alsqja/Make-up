cae670696b531da7418eb2f48f30c7e07db33c56
minseok <kms9887@naver.com>
2022-03-15 10:22:38 +0900

    [Update] Update Signup 회원가입 관련 구현사항 업데이트

---
+++ server/src/main/java/ICTPrj/server/service/SignUpService.java
@@ -0,0 +1,31 @@
+package ICTPrj.server.service;
+
+import ICTPrj.server.domain.entity.User;
+import ICTPrj.server.domain.repository.UserRepository;
+import ICTPrj.server.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SignUpService {
+    private final UserRepository userRepository;
+
+    public UserDto SignUp(UserDto userDto) {
+        if (userRepository.existsByEmail(userDto.getEmail())) {
+            throw new RuntimeException("이미 가입되어 있는 유저입니다");
+        }
+
+        User user = User.builder()
+                .nickname(userDto.getNickname())
+                .password(userDto.getPassword())
+                .email(userDto.getEmail())
+                .profile(userDto.getProfile())
+                .build();
+
+        return UserDto.of(userRepository.save(user));
+    }
+}
