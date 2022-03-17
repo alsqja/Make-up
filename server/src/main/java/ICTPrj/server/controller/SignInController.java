a106df171344caed957dfc6b3ff09e2bd9199e25
minseok <kms9887@naver.com>
2022-03-17 12:43:40 +0900

    [Update] Update files for signin 로그인 시 팔로워 및 팔로잉 수를 반환해 주기 위한 파일 업데이트

--- server/src/main/java/ICTPrj/server/controller/SignInController.java
+++ server/src/main/java/ICTPrj/server/controller/SignInController.java
@@ -1,7 +1,10 @@
 package ICTPrj.server.controller;
 
+import ICTPrj.server.dto.TokenDto;
 import ICTPrj.server.dto.TokenWithUserDto;
 import ICTPrj.server.dto.UserDto;
+import ICTPrj.server.dto.UserWithFollowDto;
+import ICTPrj.server.service.FollowService;
 import ICTPrj.server.service.SignInService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,9 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SignInController {
     private final SignInService signInService;
+    private final FollowService followService;
 
     @PostMapping("/signin")
     public TokenWithUserDto signIn(@RequestBody UserDto userDto) {
-        return signInService.signIn(userDto);
+        TokenDto accessToken = signInService.signIn(userDto);
+        UserWithFollowDto userWithFollowDto = followService.signin(userDto.getEmail());
+        TokenWithUserDto tokenWithUserDto = TokenWithUserDto.builder()
+                .accessToken(accessToken.getAccessToken())
+                .user(userWithFollowDto)
+                .build();
+        return tokenWithUserDto;
     }
 }
