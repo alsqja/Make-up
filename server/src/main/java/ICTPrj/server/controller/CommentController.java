1094e5ba2fd2b00908a83d4833f9238eafd25d88
Oh-JunYoung <qawesdxc@naver.com>
2022-03-17 10:37:34 +0900

    [Add] files for Comment
    댓글 기능 구현
    - 컨트롤러 : CommentController.java
    - 서비스 : CommentService.java
        - 댓글 작성 : writeComment
        - 댓글 수정 : modifyComment
        - 댓글 삭제 : deleteComment
    - 레포지토리
        - 댓글 : CommentRepository.java
        - 게시판 : POstRepository.java
    - Dto : CommentDto.java
    - database
        - Comment

--- server/src/main/java/ICTPrj/server/controller/CommentController.java
+++ server/src/main/java/ICTPrj/server/controller/CommentController.java
@@ -11,11 +11,26 @@ public class CommentController {
     private final CommentService commentService;
 
     @PostMapping("/post/{id}/comment")
-    public Long writeComment(@RequestHeader(value="Authorization") String userToken,
+    public Long writeComment(@RequestHeader(value = "Authorization") String userToken,
                              @PathVariable(value = "id") Long postId,
                              @RequestBody CommentDto commentDto) {
-        System.out.println("userToken = " + userToken + ", postId = " + postId + ", commentDto = " + commentDto);
         return commentService.writeComment(userToken, postId, commentDto);
     }
 
+    @PutMapping("/post/{postId}/comment/{commentId}")
+    public Long modifyComment(@RequestHeader(value = "Authorization") String userToken,
+                              @PathVariable(value = "postId") Long postId,
+                              @PathVariable(value = "commentId") Long commentId,
+                              @RequestBody CommentDto commentDto
+    ) {
+        return commentService.modifyComment(userToken, postId, commentId, commentDto);
+    }
+
+    @DeleteMapping("/post/{postId}/comment/{commentId}")
+    public Long deleteComment(@RequestHeader(value = "Authorization") String userToken,
+                              @PathVariable(value = "postId") Long postId,
+                              @PathVariable(value = "commentId") Long commentId) {
+
+        return commentService.deleteComment(userToken, postId, commentId);
+    }
 }
--- server/src/main/java/ICTPrj/server/service/CommentService.java
+++ server/src/main/java/ICTPrj/server/service/CommentService.java
@@ -9,22 +9,31 @@ import ICTPrj.server.domain.repository.UserRepository;
 import ICTPrj.server.dto.CommentDto;
 import ICTPrj.server.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CommentService {
     private final CommentRepository commentRepository;
     private final TokenProvider tokenProvider;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
 
-    public Long writeComment(String userToken, Long postId, CommentDto commentDto) {
+
+    protected User getUser(String userToken) {
         String userToken_ = userToken.substring(7);
         String userEmail = tokenProvider.getUserEmailFromToken(userToken_);
         User user = userRepository.findByEmail(userEmail).orElseThrow(() -> new IllegalArgumentException("없는 사용자입니다."));
+
+        return user;
+    }
+
+    public Long writeComment(String userToken, Long postId, CommentDto commentDto) {
+        User user = getUser(userToken);
 
         Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("없는 게시글 입니다."));
 
@@ -39,6 +48,46 @@ public class CommentService {
         return CommentDto.of(commentRepository.save(comment)).getId();
     }
 
+    public Long modifyComment(String userToken, Long postId, Long commentId, CommentDto commentDto) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다."));
+        User user = getUser(userToken);
+        Post post = postRepository.getById(postId);
 
+        if(!comment.getPost().getId().equals(postId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다.");
+        }
 
+        if(!comment.getUser().getId().equals(user.getId())) {
+            System.out.println("get Id = " + user.getId() + ", find id = " + comment.getUser().getId());
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "unauthorized");
+        }
+
+        comment = Comment.builder()
+                .id(commentId)
+                .user(user)
+                .post(post)
+                .content(commentDto.getContent())
+                .build();
+
+        return CommentDto.of(commentRepository.save(comment)).getId();
+    }
+
+    public Long deleteComment(String userToken, Long postId, Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 댓글입니다."));
+        User user = getUser(userToken);
+        Post post = postRepository.getById(postId);
+
+        if(!comment.getPost().getId().equals(postId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 댓글입니다.");
+        }
+
+        if(!comment.getUser().getId().equals(user.getId())) {
+            System.out.println("get Id = " + user.getId() + ", find id = " + comment.getUser().getId());
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "unauthorized");
+        }
+
+        commentRepository.delete(comment);
+
+        return commentId;
+    }
 }
