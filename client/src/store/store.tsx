bba9565e285828691bda3dbf097fca9ff8f1577a
yjlim0428 <yjlim9898@gmail.com>
2022-03-17 23:26:26 +0900

    [Edit] SettingModal, MyPage

--- client/src/store/store.tsx
+++ client/src/store/store.tsx
@@ -1,16 +1,21 @@
 import { atom } from "recoil";
 
 export const isLogin = atom<boolean>({
-  key: 'isLogin',
-  default: false
-})
+  key: "isLogin",
+  default: false,
+});
 
 export const followModal = atom<boolean>({
-  key: 'followModal',
-  default: false
-})
+  key: "followModal",
+  default: false,
+});
 
 export const followerModal = atom<boolean>({
-  key: 'followerModal',
-  default: false
-})
+  key: "followerModal",
+  default: false,
+});
+
+export const userSettingModal = atom<boolean>({
+  key: "userSettingModal",
+  default: false,
+});
