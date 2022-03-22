39d8ead84ec9a3d60c78313326e8922ca2cf5212
Oh-JunYoung <qawesdxc@naver.com>
2022-03-22 14:22:08 +0900

    [Add] files for test 유저 회원가입, 로그인 기능 테스트 추가 - 테스트 : SignInTest, SignUpTest

--- server/src/test/java/ICTPrj/server/SignUpTest.java
+++ server/src/test/java/ICTPrj/server/SignUpTest.java
@@ -1,37 +1,36 @@
-//package ICTPrj.server;
-//
-//import ICTPrj.server.domain.entity.User;
-//import ICTPrj.server.domain.repository.UserRepository;
-//import ICTPrj.server.dto.UserDto;
-//import ICTPrj.server.service.SignUpService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//
-//@SpringBootTest
-//@Transactional
-//public class SignUpTest {
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    @Autowired
-//    SignUpService signUpService;
-//
-//    @Test
-//    public void SignUp() throws Exception {
-//        // Given
-//        UserDto userDto = new UserDto();
-//        userDto.setEmail("abc@naver.com");
-//        userDto.setNickname("test");
-//        userDto.setPassword("123");
-//        userDto.setProfile("hi");
-//
-//        // when
-//        Long userID = signUpService.SignUp(userDto);
-//
-//        // then
-//        System.out.println(userID);
-//    }
-//}
+package ICTPrj.server;
+
+import ICTPrj.server.dto.UserDto;
+import ICTPrj.server.service.SignUpService;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SignUpTest {
+    @Autowired
+    private SignUpService signUpService;
+
+    @Test
+//    @Order(1)
+    @DisplayName("회원가입 테스트")
+    public void SignUp() throws Exception {
+        // Given
+        UserDto userDto = UserDto.builder()
+                .email("test@naver.com")
+                .nickname("test")
+                .password("123")
+                .profile("test").build();
+
+        // when
+        UserDto newUser = signUpService.SignUp(userDto);
+
+        // then
+        assertEquals(userDto.getEmail(), newUser.getEmail());
+    }
+}
